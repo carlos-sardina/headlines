933cd70dd33816949ece47939dfb7ac48f414976
carlos-sardina <developerAlbertoMT@gmail.com>
2024-01-31 18:39:22 -0600

    updating headline test

--- src/components/Headline/Headline.test.tsx
+++ src/components/Headline/Headline.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from '@test-utils';
 import { Headline } from './Headline';
-import moment from 'moment';
 import { Headline as THeadline } from '@types';
 
 describe('Headline', () => {
@@ -10,15 +9,25 @@ describe('Headline', () => {
     publishedAt: new Date().toISOString(),
     source: { name: 'Test Source' },
     url: 'https://test.com',
-    country: 'us'
+    country: 'us',
+    urlToImage: 'https://test.com'
   };
 
-  test('renders Headline component with correct author, title, published date, and source name', () => {
+  test('renders author when author exists', () => {
     render(<Headline headline={headline} />);
     expect(screen.getByText(headline.author || '')).toBeInTheDocument();
-    expect(screen.getByText(headline.title || '')).toBeInTheDocument();
-    expect(screen.getByText(moment(headline.publishedAt).fromNow())).toBeInTheDocument();
-    expect(screen.getByText(headline.source.name || '')).toBeInTheDocument();
+  });
+
+  test('does not render author when author is null', () => {
+    const headlineWithoutAuthor = {
+      ...headline,
+      author: undefined,
+      publishedAt: undefined,
+      source: { name: undefined },
+      country: undefined,
+      urlToImage: undefined
+    };
+    render(<Headline headline={headlineWithoutAuthor} />);
   });
 
   test('does not render when title is [Removed]', () => {
