a5a8cfb8bdadaa406bbab579a2e835b54ccaf87b
carlos-sardina <developerAlbertoMT@gmail.com>
2024-01-31 22:36:07 -0600

    SEO

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
 import { Headline, Pagination } from '@components';
+import { CATEGORIES_MAP, COUNTRIES_MAP } from '@constants';
 import { getCategoryParam, getCountryParam, getPageParam, getHeadlines } from '@utils';
+import { Metadata } from 'next';
 
 type Props = {
   searchParams: URLSearchParams;
 };
+
+export async function generateMetadata({ searchParams }: Props): Promise<Metadata> {
+  const params = new URLSearchParams(searchParams);
+  const country = getCountryParam(params);
+  const countryLabel = COUNTRIES_MAP[country].label;
+  const category = getCategoryParam(params);
+  const categoryLabel = category === CATEGORIES_MAP.all.key ? '' : CATEGORIES_MAP[category].label;
+
+  return {
+    title: `Headlines - Latest ${categoryLabel} News from ${countryLabel}`,
+    description: `Stay updated with the latest ${categoryLabel.toLowerCase()} news from ${countryLabel}`,
+    openGraph: {
+      title: `Headlines - Latest ${categoryLabel} News from ${countryLabel}`,
+      description: `Stay updated with the latest ${categoryLabel.toLowerCase()} news from ${countryLabel}`
+    }
+  };
+}
 
 export default async function Home({ searchParams }: Props) {
   const params = new URLSearchParams(searchParams);
